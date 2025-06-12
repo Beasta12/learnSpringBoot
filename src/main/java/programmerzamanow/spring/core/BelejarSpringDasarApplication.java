6b5b7a9b79b0560c7a0cc7233414774cb800ee60
DESKTOP-UR1OUJN\USER <jovan.hariono@pancabudi.com>
2025-06-12 11:19:25 +0700

    first commit

---
+++ src/main/java/programmerzamanow/spring/core/BelejarSpringDasarApplication.java
@@ -0,0 +1,13 @@
+package programmerzamanow.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelejarSpringDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelejarSpringDasarApplication.class, args);
+	}
+
+}
